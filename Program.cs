e9393a07d230531f4305b7569033c9242b2f28bd
Warrior Divs <145162449+warriordivs@users.noreply.github.com>
2023-09-16 23:11:56 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-using System;
+﻿using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        //Below line is added to make the output screen wait for user to enter a key so that we can see the output
+        Console.ReadKey();
+    }
 }
